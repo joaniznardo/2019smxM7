88d01bd3dbff22ac735dbeda637f956416c7966a
joaniznardo <joan.iznardo@iesjoandaustria.org>
2019-12-05 13:00:15 +0100

    restauració de volums: 2 exemples

---
+++ complements/docker/demo_us_volums/demo-restore-volume.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+## reatauració (traspàs) d'un fitxer de la màquina física a un volum
+## procés per etapes
+
+## 
+NOM_VOLUM=volum-restore
+NOM_CONTAINER=container-auxiliar
+DIRECTORI_DE_CONTAINER=unnomqualsevol
+DIRECTORI_TEMPORAL=tmp
+FITXER_A_TRASSPASSAR=millor-un-tar.tgz
+FITXER_A_EMPAQUETAR=un-nom-qualsevol.txt
+
+# idempotent: eliminem abans de crear
+docker container rm $NOM_CONTAINER
+docker volume rm $NOM_VOLUM
+
+# crear el volum
+docker volume create $NOM_VOLUM
+
+# crear un fitxer a la màquina física
+echo "mmmmm. Que hi ha, mestre?" | tee $FITXER_A_EMPAQUETAR
+
+# crear un fitxer empaquetat (i comprimit)
+tar -czvf $FITXER_A_TRASSPASSAR $FITXER_A_EMPAQUETAR 
+
+# crear un container (amb nom) associant-li un volum a una carpeta (inexistent) i execució d'un intèrpret d'ordres (/bin/sh) en segon planol (-d) 
+## nota: se podrem connectar a ell després amb un "docker container attach $NOM_CONTAINER" i tornar a abandonar-lo amb ^+P+Q
+docker container run -dit --name $NOM_CONTAINER -v $NOM_VOLUM:/$DIRECTORI_DE_CONTAINER ubuntu:trusty /bin/sh
+
+# còpia entre màquina física i container
+docker container cp $FITXER_A_TRASSPASSAR $NOM_CONTAINER:/$DIRECTORI_TEMPORAL 
+
+# desempaquetar al lloc on ens interessa
+docker container exec $NOM_CONTAINER tar -xzvf /$DIRECTORI_TEMPORAL/$FITXER_A_TRASSPASSAR -C /$DIRECTORI_DE_CONTAINER
---
+++ complements/docker/demo_us_volums/demo2-restore-volume.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+## -- comprovar al fitxer "demo2.env" el valor de les variables d'entorn
+### NOM_VOLUM=volum-restore
+### NOM_CONTAINER=container-auxiliar
+### DIRECTORI_DE_CONTAINER=unnomqualsevol
+### DIRECTORI_TEMPORAL=tmp
+### FITXER_A_TRASSPASSAR=millor-un-tar.tgz
+### FITXER_A_EMPAQUETAR=un-nom-qualsevol.txt
+
+
+# incorporem les variables d'entorn d'un fitxer: haurien de ser les que apareixen a dalt comentades
+source demo2.env
+
+# idempotent: eliminem abans de crear
+docker container rm $NOM_CONTAINER
+docker volume rm $NOM_VOLUM
+
+# creem volum
+docker volume create $NOM_VOLUM
+
+# creem un fitxer
+echo "mmmmm. Que hi ha, mestre?" | tee $FITXER_A_EMPAQUETAR
+
+# empaquetem un fitxer en un altre
+tar -czvf $FITXER_A_TRASSPASSAR $FITXER_A_EMPAQUETAR 
+
+# executem directament l'ordre de descomprimir en el container en un directori concret, agafant el ".tgz" de l'entrada estàndar ( -f - ), gràcies a la redirecció a procés ( "|")
+# aquí la "màgia és que la sortida d'un procés de la màquina física (cat), s'aprofita en un procés del container (tar)
+cat $FITXER_A_TRASSPASSAR | docker container run -i --env-file ./demo2.env  --name $NOM_CONTAINER -v $NOM_VOLUM:/$DIRECTORI_DE_CONTAINER alpine:latest tar -xvzf - -C /$DIRECTORI_DE_CONTAINER
