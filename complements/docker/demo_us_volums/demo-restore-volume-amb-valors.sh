b737b73e5393a36764e6bfe2b68b7e4d56b86b8d
joaniznardo <joan.iznardo@iesjoandaustria.org>
2019-12-09 12:13:45 +0100

    Invocació correcta de envsubst + assignació correcta variables

--- complements/docker/demo_us_volums/demo-restore-volume-amb-valors.sh
+++ complements/docker/demo_us_volums/demo-restore-volume-amb-valors.sh
@@ -12,24 +12,24 @@ FITXER_A_EMPAQUETAR="un-nom-qualsevol.txt"
 
 ##source demo2.env
 # idempotent: eliminem abans de crear
-docker container rm $NOM_CONTAINER
-docker volume rm $NOM_VOLUM
+docker container rm container-auxiliar3
+docker volume rm volum-restore3
 
 # crear el volum
-docker volume create $NOM_VOLUM
+docker volume create volum-restore3
 
 # crear un fitxer a la màquina física
-echo "mmmmm. Que hi ha, mestre?" | tee $FITXER_A_EMPAQUETAR
+echo "mmmmm. Que hi ha, mestre?" | tee un-nom-qualsevol3.txt
 
 # crear un fitxer empaquetat (i comprimit)
-tar -czvf $FITXER_A_TRASSPASSAR $FITXER_A_EMPAQUETAR 
+tar -czvf flipa-en-colors.tgz un-nom-qualsevol3.txt 
 
 # crear un container (amb nom) associant-li un volum a una carpeta (inexistent) i execució d'un intèrpret d'ordres (/bin/sh) en segon planol (-d) 
-## nota: se podrem connectar a ell després amb un "docker container attach $NOM_CONTAINER" i tornar a abandonar-lo amb ^+P+Q
-docker container run -dit --name $NOM_CONTAINER -v $NOM_VOLUM:/$DIRECTORI_DE_CONTAINER ubuntu:trusty /bin/sh
+## nota: se podrem connectar a ell després amb un "docker container attach container-auxiliar3" i tornar a abandonar-lo amb ^+P+Q
+docker container run -dit --name container-auxiliar3 -v volum-restore3:/unnomqualsevol3 ubuntu:trusty /bin/sh
 
 # còpia entre màquina física i container
-docker container cp $FITXER_A_TRASSPASSAR $NOM_CONTAINER:/$DIRECTORI_TEMPORAL 
+docker container cp flipa-en-colors.tgz container-auxiliar3:/tmp 
 
 # desempaquetar al lloc on ens interessa
-docker container exec $NOM_CONTAINER tar -xzvf /$DIRECTORI_TEMPORAL/$FITXER_A_TRASSPASSAR -C /$DIRECTORI_DE_CONTAINER
+docker container exec container-auxiliar3 tar -xzvf /tmp/flipa-en-colors.tgz -C /unnomqualsevol3
