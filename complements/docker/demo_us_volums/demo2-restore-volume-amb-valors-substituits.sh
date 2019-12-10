314054afb2a06df9cb54514600c12a052e4f00b8
joaniznardo <joan.iznardo@iesjoandaustria.org>
2019-12-10 09:44:42 +0100

    dependències intra Makefile

--- complements/docker/demo_us_volums/demo2-restore-volume-amb-valors-substituits.sh
+++ complements/docker/demo_us_volums/demo2-restore-volume-amb-valors-substituits.sh
@@ -13,18 +13,18 @@
 source demo2.env
 
 # idempotent: eliminem abans de crear
-docker container rm container-auxiliar
-docker volume rm volum-restore
+docker container rm container-auxiliar3
+docker volume rm volum-restore3
 
 # creem volum
-docker volume create volum-restore
+docker volume create volum-restore3
 
 # creem un fitxer
-echo "mmmmm. Que hi ha, mestre?" | tee un-nom-qualsevol.txt
+echo "mmmmm. Que hi ha, mestre?" | tee un-nom-qualsevol3.txt
 
 # empaquetem un fitxer en un altre
-tar -czvf millor-un-tar.tgz un-nom-qualsevol.txt 
+tar -czvf flipa-en-colors.tgz un-nom-qualsevol3.txt 
 
 # executem directament l'ordre de descomprimir en el container en un directori concret, agafant el ".tgz" de l'entrada estàndar ( -f - ), gràcies a la redirecció a procés ( "|")
 # aquí la "màgia és que la sortida d'un procés de la màquina física (cat), s'aprofita en un procés del container (tar)
-cat millor-un-tar.tgz | docker container run -i --env-file ./demo2.env  --name container-auxiliar -v volum-restore:/unnomqualsevol alpine:latest tar -xvzf - -C /unnomqualsevol
+cat flipa-en-colors.tgz | docker container run -i --env-file ./demo2.env  --name container-auxiliar3 -v volum-restore3:/unnomqualsevol3 alpine:latest tar -xvzf - -C /unnomqualsevol3
